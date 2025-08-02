fe8cdb419c72c2290ce44201965c2260a53a3b3d
TheBestSoftwareEngineer <MOHAMEDHAMDY192001@gmail.com>
2025-08-02 22:28:38 +0300

    enhance quiz screen ui

--- lib/features/quiz_screen/presentation/views/widgets/quiz_question_card.dart
+++ lib/features/quiz_screen/presentation/views/widgets/quiz_question_card.dart
@@ -4,10 +4,10 @@ import '../../../../../../core/utils/constants.dart';
 import '../../../../../../core/utils/styles.dart';
 import '../../../../../core/utils/responsive_helper.dart';
 
-class QuizQuestionCard extends StatelessWidget {
+class QuizQuestionCard extends StatefulWidget {
   final QuestionWithChoices question;
   final Size size;
-  
+
   const QuizQuestionCard({
     super.key,
     required this.question,
@@ -15,37 +15,98 @@ class QuizQuestionCard extends StatelessWidget {
   });
 
   @override
-  Widget build(BuildContext context) {
-    final cardPadding = ResponsiveHelper.getPadding(size);
+  State<QuizQuestionCard> createState() => _QuizQuestionCardState();
+}
+
+class _QuizQuestionCardState extends State<QuizQuestionCard>
+    with SingleTickerProviderStateMixin {
+  late AnimationController _animationController;
+  late Animation<double> _slideAnimation;
+  late Animation<double> _fadeAnimation;
+
+  @override
+  void initState() {
+    super.initState();
+    _animationController = AnimationController(
+      duration: const Duration(milliseconds: 600),
+      vsync: this,
+    );
     
-    return Container(
-      width: double.infinity,
-      padding: EdgeInsets.all(cardPadding * 1.5),
-      decoration: BoxDecoration(
-        color: backgroundBoxesColor,
-        borderRadius: BorderRadius.circular(
-          ResponsiveHelper.getBorderRadius(size, 16),
-        ),
-        boxShadow: [
-          BoxShadow(
-            color: Colors.black.withOpacity(0.05),
-            blurRadius: 10,
-            offset: const Offset(0, 2),
+    _slideAnimation = Tween<double>(
+      begin: 30.0,
+      end: 0.0,
+    ).animate(CurvedAnimation(
+      parent: _animationController,
+      curve: Curves.easeOutCubic,
+    ));
+    
+    _fadeAnimation = Tween<double>(
+      begin: 0.0,
+      end: 1.0,
+    ).animate(CurvedAnimation(
+      parent: _animationController,
+      curve: Curves.easeInOut,
+    ));
+    
+    _animationController.forward();
+  }
+
+  @override
+  void dispose() {
+    _animationController.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    final cardPadding = ResponsiveHelper.getPadding(widget.size);
+    final isTablet = ResponsiveHelper.isTablet(widget.size);
+
+    return AnimatedBuilder(
+      animation: _animationController,
+      builder: (context, child) {
+        return Transform.translate(
+          offset: Offset(0, _slideAnimation.value),
+          child: FadeTransition(
+            opacity: _fadeAnimation,
+            child: Container(
+              width: double.infinity,
+              margin: EdgeInsets.symmetric(horizontal: cardPadding * 0.2),
+              padding: EdgeInsets.all(cardPadding * 1.5),
+              decoration: BoxDecoration(
+                color: Colors.white,
+                borderRadius: BorderRadius.circular(
+                  ResponsiveHelper.getBorderRadius(widget.size, 16),
+                ),
+                border: Border.all(
+                  color: mainColor.withOpacity(0.2),
+                  width: 2,
+                ),
+                boxShadow: [
+                  BoxShadow(
+                    color: Colors.black.withOpacity(0.05),
+                    blurRadius: 10,
+                    offset: const Offset(0, 2),
+                  ),
+                ],
+              ),
+              child: Directionality(
+                textDirection: TextDirection.rtl,
+                child: Text(
+                  widget.question.questionText,
+                  style: Styles.mainBlackText18.copyWith(
+                    fontSize: ResponsiveHelper.getFontSize(widget.size, isTablet ? 20 : 18),
+                    height: 1.5,
+                    fontWeight: FontWeight.w600,
+                    color: const Color(0xFF1A1A1A),
+                  ),
+                  textAlign: TextAlign.right,
+                ),
+              ),
+            ),
           ),
-        ],
-      ),
-      child: Directionality(
-        textDirection: TextDirection.rtl,
-        child: Text(
-          question.questionText,
-          style: Styles.mainBlackText18.copyWith(
-            fontSize: ResponsiveHelper.getFontSize(size, 18),
-            height: 1.5,
-            fontWeight: FontWeight.w500,
-          ),
-          textAlign: TextAlign.right,
-        ),
-      ),
+        );
+      },
     );
   }
 }
--- lib/features/quiz_screen/presentation/views/widgets/quiz_submit_button.dart
+++ lib/features/quiz_screen/presentation/views/widgets/quiz_submit_button.dart
@@ -2,7 +2,7 @@ import 'package:flutter/material.dart';
 import '../../../../../../core/utils/constants.dart';
 import '../../../../../core/utils/responsive_helper.dart';
 
-class QuizSubmitButton extends StatelessWidget {
+class QuizSubmitButton extends StatefulWidget {
   final bool hasSelectedAnswer;
   final bool isAnswered;
   final bool isSubmitting;
@@ -19,51 +19,237 @@ class QuizSubmitButton extends StatelessWidget {
   });
 
   @override
-  Widget build(BuildContext context) {
-    final isButtonEnabled = hasSelectedAnswer && !isAnswered && !isSubmitting;
-    final isTablet = ResponsiveHelper.isTablet(size);
-    final isLargeScreen = ResponsiveHelper.isLargeScreen(size);
-    final padding = ResponsiveHelper.getPadding(size);
+  State<QuizSubmitButton> createState() => _QuizSubmitButtonState();
+}
 
-    return Padding(
-      padding: EdgeInsets.fromLTRB(padding, 0, padding, padding),
-      child: Center(
-        child: Container(
-          constraints: BoxConstraints(
-            maxWidth: isLargeScreen ? 600 : (isTablet ? 400 : double.infinity),
+class _QuizSubmitButtonState extends State<QuizSubmitButton>
+    with TickerProviderStateMixin {
+  late AnimationController _pulseController;
+  late AnimationController _scaleController;
+  late Animation<double> _pulseAnimation;
+  late Animation<double> _scaleAnimation;
+
+  @override
+  void initState() {
+    super.initState();
+    
+    _pulseController = AnimationController(
+      duration: const Duration(milliseconds: 1200),
+      vsync: this,
+    );
+    
+    _scaleController = AnimationController(
+      duration: const Duration(milliseconds: 150),
+      vsync: this,
+    );
+    
+    _pulseAnimation = Tween<double>(
+      begin: 1.0,
+      end: 1.05,
+    ).animate(CurvedAnimation(
+      parent: _pulseController,
+      curve: Curves.easeInOut,
+    ));
+    
+    _scaleAnimation = Tween<double>(
+      begin: 1.0,
+      end: 0.95,
+    ).animate(CurvedAnimation(
+      parent: _scaleController,
+      curve: Curves.easeInOut,
+    ));
+    
+    // Start pulsing when button becomes enabled
+    if (widget.hasSelectedAnswer && !widget.isAnswered) {
+      _pulseController.repeat(reverse: true);
+    }
+  }
+
+  @override
+  void didUpdateWidget(QuizSubmitButton oldWidget) {
+    super.didUpdateWidget(oldWidget);
+    
+    // Control pulsing animation based on button state
+    if (widget.hasSelectedAnswer && !widget.isAnswered && !widget.isSubmitting) {
+      _pulseController.repeat(reverse: true);
+    } else {
+      _pulseController.stop();
+      _pulseController.reset();
+    }
+  }
+
+  @override
+  void dispose() {
+    _pulseController.dispose();
+    _scaleController.dispose();
+    super.dispose();
+  }
+
+  void _onTapDown(TapDownDetails details) {
+    _scaleController.forward();
+  }
+
+  void _onTapUp(TapUpDetails details) {
+    _scaleController.reverse();
+  }
+
+  void _onTapCancel() {
+    _scaleController.reverse();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    final isButtonEnabled = widget.hasSelectedAnswer && !widget.isAnswered && !widget.isSubmitting;
+    final isTablet = ResponsiveHelper.isTablet(widget.size);
+    final isLargeScreen = ResponsiveHelper.isLargeScreen(widget.size);
+    final padding = ResponsiveHelper.getPadding(widget.size);
+
+    return Container(
+      decoration: BoxDecoration(
+        gradient: LinearGradient(
+          begin: Alignment.topCenter,
+          end: Alignment.bottomCenter,
+          colors: [
+            Colors.white.withOpacity(0.8),
+            Colors.white,
+          ],
+        ),
+        boxShadow: [
+          BoxShadow(
+            color: Colors.black.withOpacity(0.08),
+            blurRadius: 12,
+            offset: const Offset(0, -2),
           ),
-          child: SizedBox(
-            width: double.infinity,
-            height: isTablet ? 64 : 56,
-            child: ElevatedButton(
-              onPressed: isButtonEnabled ? onSubmit : null,
-              style: ElevatedButton.styleFrom(
-                backgroundColor: isButtonEnabled ? mainColor : Colors.grey,
-                foregroundColor: Colors.white,
-                elevation: 0,
-                shape: RoundedRectangleBorder(
-                  borderRadius: BorderRadius.circular(
-                    ResponsiveHelper.getBorderRadius(size, 16),
-                  ),
-                ),
-              ),
-              child: isSubmitting
-                  ? SizedBox(
-                      width: isTablet ? 28 : 24,
-                      height: isTablet ? 28 : 24,
-                      child: const CircularProgressIndicator(
-                        strokeWidth: 2,
-                        valueColor: AlwaysStoppedAnimation<Color>(Colors.white),
+        ],
+      ),
+      child: Padding(
+        padding: EdgeInsets.fromLTRB(padding, padding * 0.8, padding, padding * 1.2),
+        child: Center(
+          child: Container(
+            constraints: BoxConstraints(
+              maxWidth: isLargeScreen ? 600 : (isTablet ? 400 : double.infinity),
+            ),
+            child: AnimatedBuilder(
+              animation: Listenable.merge([_pulseAnimation, _scaleAnimation]),
+              builder: (context, child) {
+                return Transform.scale(
+                  scale: _scaleAnimation.value * (isButtonEnabled ? _pulseAnimation.value : 1.0),
+                  child: Container(
+                    width: double.infinity,
+                    height: isTablet ? 68 : 60,
+                    decoration: BoxDecoration(
+                      gradient: isButtonEnabled
+                          ? LinearGradient(
+                              begin: Alignment.topLeft,
+                              end: Alignment.bottomRight,
+                              colors: [
+                                mainColor,
+                                mainColor.withOpacity(0.8),
+                              ],
+                            )
+                          : LinearGradient(
+                              colors: [
+                                Colors.grey.withOpacity(0.6),
+                                Colors.grey.withOpacity(0.4),
+                              ],
+                            ),
+                      borderRadius: BorderRadius.circular(
+                        ResponsiveHelper.getBorderRadius(widget.size, 18),
                       ),
-                    )
-                  : Text(
-                      isAnswered ? 'تم الإجابة' : 'تأكيد الإجابة',
-                      style: TextStyle(
-                        fontSize: ResponsiveHelper.getFontSize(size, 18),
-                        color: Colors.white,
-                        fontWeight: FontWeight.w600,
+                      boxShadow: isButtonEnabled
+                          ? [
+                              BoxShadow(
+                                color: mainColor.withOpacity(0.3),
+                                blurRadius: 16,
+                                offset: const Offset(0, 8),
+                                spreadRadius: 0,
+                              ),
+                              BoxShadow(
+                                color: mainColor.withOpacity(0.1),
+                                blurRadius: 24,
+                                offset: const Offset(0, 12),
+                                spreadRadius: -4,
+                              ),
+                            ]
+                          : [
+                              BoxShadow(
+                                color: Colors.grey.withOpacity(0.2),
+                                blurRadius: 8,
+                                offset: const Offset(0, 4),
+                              ),
+                            ],
+                    ),
+                    child: Material(
+                      color: Colors.transparent,
+                      child: InkWell(
+                        onTap: isButtonEnabled ? widget.onSubmit : null,
+                        onTapDown: isButtonEnabled ? _onTapDown : null,
+                        onTapUp: isButtonEnabled ? _onTapUp : null,
+                        onTapCancel: isButtonEnabled ? _onTapCancel : null,
+                        borderRadius: BorderRadius.circular(
+                          ResponsiveHelper.getBorderRadius(widget.size, 18),
+                        ),
+                        child: Container(
+                          alignment: Alignment.center,
+                          child: widget.isSubmitting
+                              ? Row(
+                                  mainAxisAlignment: MainAxisAlignment.center,
+                                  children: [
+                                    SizedBox(
+                                      width: isTablet ? 24 : 20,
+                                      height: isTablet ? 24 : 20,
+                                      child: CircularProgressIndicator(
+                                        strokeWidth: 2.5,
+                                        valueColor: const AlwaysStoppedAnimation<Color>(Colors.white),
+                                      ),
+                                    ),
+                                    SizedBox(width: widget.size.width * 0.03),
+                                    Text(
+                                      'جاري التحميل...',
+                                      style: TextStyle(
+                                        fontSize: ResponsiveHelper.getFontSize(widget.size, 16),
+                                        color: Colors.white,
+                                        fontWeight: FontWeight.w600,
+                                      ),
+                                    ),
+                                  ],
+                                )
+                              : Row(
+                                  mainAxisAlignment: MainAxisAlignment.center,
+                                  children: [
+                                    if (isButtonEnabled && !widget.isAnswered) ...[
+                                      Icon(
+                                        Icons.check_circle_outline,
+                                        color: Colors.white,
+                                        size: ResponsiveHelper.getFontSize(widget.size, 20),
+                                      ),
+                                      SizedBox(width: widget.size.width * 0.02),
+                                    ],
+                                    if (widget.isAnswered) ...[
+                                      Icon(
+                                        Icons.done_all,
+                                        color: Colors.white,
+                                        size: ResponsiveHelper.getFontSize(widget.size, 20),
+                                      ),
+                                      SizedBox(width: widget.size.width * 0.02),
+                                    ],
+                                    Text(
+                                      widget.isAnswered ? 'تم الإجابة' : 'تأكيد الإجابة',
+                                      style: TextStyle(
+                                        fontSize: ResponsiveHelper.getFontSize(widget.size, 18),
+                                        color: Colors.white,
+                                        fontWeight: FontWeight.w600,
+                                        letterSpacing: 0.5,
+                                      ),
+                                    ),
+                                  ],
+                                ),
+                        ),
                       ),
                     ),
+                  ),
+                );
+              },
             ),
           ),
         ),
